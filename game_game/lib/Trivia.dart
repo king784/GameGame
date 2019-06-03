fab253f45470d945c6cc88136ce7281715d2bf22
RikuH <45202322+RikuH@users.noreply.github.com>
2019-06-03 13:18:59 +0300

    KYSymykset tulee eri teksti lähteestä
    KYSymykset tulee eri teksti lähteestä

---
+++ game_game/lib/Trivia.dart
@@ -0,0 +1,408 @@
+import 'main.dart';
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+import 'package:flutter/services.dart' show rootBundle;
+import 'dart:async' show Future;
+import 'package:path/path.dart' as p;
+import "dart:io";
+import 'package:path_provider/path_provider.dart';
+
+class ktpQuiz{
+
+  var images = [
+    "Halli", "pepe2", "pepe3", "Halli2", "kentta"];
+
+
+  var choices=[
+    ["Kotkan uimahalli", "Steveco areena", "Eduskunta talo", "Karhuvuoren koulu"],
+    ["10", "8", "5", "12"],
+    ["1", "24", "99", "14"],
+    ["8", "6", "4", "10"],
+    ["25 x 14", "29 x 16", "28 x 15", "31 x 17"]
+  ];
+
+  var correctAnswers=[
+    "Steveco areena", "10", "14", "4", "28 x 15"
+  ];
+}
+
+String getContents;
+
+var finalScore = 0;
+var questionNumber = 0;
+var quiz = new ktpQuiz();
+List<int> randNums = [0, 1, 2, 3];
+int rand1, rand2, rand3, rand4;
+bool firstRun = true;
+
+
+class Trivia extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState(){
+    return new TriviaState();
+  }
+}
+
+
+class TriviaState extends State<Trivia> {
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery
+        .of(context)
+        .size
+        .width;
+    double screenHeight = MediaQuery
+        .of(context)
+        .size
+        .height;
+
+    //Style variables
+    double buttonsWidth = screenWidth / 3;
+    double buttonHeight = screenHeight / 10;
+
+    Startade();
+    List<List<String>> quizContents = new List<List<String>>();
+    quizContents.add(globals.contents.split(";"));
+
+    // TODO: implement build
+    return new WillPopScope(
+        child: Scaffold(
+          body: new Container(
+            decoration: new BoxDecoration(
+              image: new DecorationImage(
+                  image: new AssetImage("images/backGround.jpg"),
+                  colorFilter: new ColorFilter.mode(
+                      Colors.white.withOpacity(0.15), BlendMode.dstATop)
+                //fit: BoxFit.cover,
+              ),
+            ),
+            //margin: const EdgeInsets.all(10.0),
+            alignment: Alignment.topCenter,
+            child: new Column(
+              children: <Widget>[
+
+
+                //new Text(readFromFile,
+                new Text(quizContents[0][questionNumber],
+                //new Text(quiz.questions[questionNumber],
+                  textAlign: TextAlign.center,
+                  style: new TextStyle(
+                    fontSize: screenHeight / 25,
+                  ),
+                ),
+
+                new Image.asset("images/${quiz.images[questionNumber]}.jpg",
+                  height: screenHeight / 2.5, width: screenWidth,),
+
+                new Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+
+                    //
+                    //Buttons
+                    //
+
+                    //Button1
+                    new MaterialButton(
+                      padding: EdgeInsets.all(10),
+                      minWidth: buttonsWidth,
+                      height: buttonHeight,
+                      color: Colors.black12,
+                      onPressed: () {
+                        if (quiz.choices[questionNumber][rand1] ==
+                            quiz.correctAnswers[questionNumber]) {
+                          debugPrint("Correct!");
+                          finalScore++;
+                        }
+                        else {
+                          print(quiz.correctAnswers[questionNumber]);
+                        }
+                        updateQuestion();
+                      },
+                      child: new Text(
+                        quiz.choices[questionNumber][rand1],
+                        style: new TextStyle(
+                            fontSize: buttonsWidth / 7,
+                            color: Colors.white
+                        ),
+                      ),
+                    ),
+
+                    //Button2
+                    new MaterialButton(
+                      padding: EdgeInsets.all(10),
+                      minWidth: buttonsWidth,
+                      height: buttonHeight,
+                      color: Colors.black12,
+                      onPressed: () {
+                        if (quiz.choices[questionNumber][rand2] ==
+                            quiz.correctAnswers[questionNumber]) {
+                          debugPrint("Correct!");
+                          finalScore++;
+                        }
+                        else {
+                          print(quiz.correctAnswers[questionNumber]);
+                        }
+                        updateQuestion();
+                      },
+                      child: new Text(
+                        quiz.choices[questionNumber][rand2],
+                        style: new TextStyle(
+                            fontSize: buttonsWidth / 7,
+                            color: Colors.white
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                new Row(children: <Widget>[new Text(" ")],),
+                new Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+
+                    //Button3
+                    new MaterialButton(
+                      padding: EdgeInsets.all(10),
+                      minWidth: buttonsWidth,
+                      height: buttonHeight,
+                      color: Colors.black12,
+                      onPressed: () {
+                        if (quiz.choices[questionNumber][rand3] ==
+                            quiz.correctAnswers[questionNumber]) {
+                          debugPrint("Correct!");
+                          finalScore++;
+                        }
+                        else {
+                          print(quiz.correctAnswers[questionNumber]);
+                        }
+                        updateQuestion();
+                      },
+                      child: new Text(
+                        quiz.choices[questionNumber][rand3],
+                        style: new TextStyle(
+                            fontSize: buttonsWidth / 7,
+                            color: Colors.white
+                        ),
+                      ),
+                    ),
+
+
+                    //Button4
+                    new MaterialButton(
+                      padding: EdgeInsets.all(10),
+                      minWidth: buttonsWidth,
+                      height: buttonHeight,
+                      color: Colors.black12,
+                      onPressed: () {
+                        if (quiz.choices[questionNumber][rand4] ==
+                            quiz.correctAnswers[questionNumber]) {
+                          debugPrint("Correct!");
+                          finalScore++;
+                        }
+                        else {
+                          print(quiz.correctAnswers[questionNumber]);
+                        }
+                        updateQuestion();
+                      },
+                      child:
+                      new Text(quiz.choices[questionNumber][rand4],
+                        style: new TextStyle(
+                            fontSize: buttonsWidth / 7,
+                            color: Colors.white
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                new Row(children: <Widget>[new Text(" ")],),
+                new Container(
+                  alignment: Alignment.bottomCenter,
+                  child: new MaterialButton(
+                    color: Colors.red,
+                    minWidth: buttonsWidth,
+                    height: buttonHeight,
+                    onPressed: resetQuiz,
+                    child: new Text("Quit",
+                      style: new TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white
+                      ),
+                    ),
+
+                  ),
+                )
+
+              ],
+            ),
+          ),
+        ),
+        onWillPop: () async => false);
+  }
+
+  void Startade() async{
+
+    print(globals.contents.split(";").length);
+    if(firstRun)
+    {
+      //loadQuestions();
+      //await loadQuestions();
+      RandomizeQuestions();
+      ReAddValues();
+      firstRun = false;
+    }
+  }
+
+  void resetQuiz() {
+    setState(() {
+      Navigator.pop(context);
+      finalScore = 0;
+      questionNumber = 0;
+    });
+  }
+
+
+  void updateQuestion() {
+    setState(() {
+      print(globals.contents.length);
+      if (questionNumber == globals.contents.split(";").length - 1) {
+        Navigator.push(context, new MaterialPageRoute(
+            builder: (context) => new Summary(score: finalScore)));
+      }
+      else {
+        questionNumber++;
+        RandomizeQuestions();
+        ReAddValues();
+      }
+    });
+  }
+
+  void RandomizeQuestions()
+  {
+    rand1 = randNums.removeAt(Random().nextInt(randNums.length));
+    rand2 = randNums.removeAt(Random().nextInt(randNums.length));
+    rand3 = randNums.removeAt(Random().nextInt(randNums.length));
+    rand4 = randNums.removeAt(Random().nextInt(randNums.length));
+  }
+
+  void ReAddValues()
+  {
+    randNums.clear();
+    for(int i = 0; i < 4; i++)
+    {
+      randNums.add(i);
+    }
+  }
+}
+
+String scoreText(final int score){
+  double percentage = (((score).toDouble()) / globals.contents.split(";").length) * 100;
+
+  if(percentage <= 15)
+    return "Harmillista. Sait vain $score pistettä.";
+
+  else if(percentage >=16 && percentage <= 45)
+    return "Noh.. Ainahan sitä voi vähän petrata. Pisteesi: $score";
+
+  else if(percentage >=46 && percentage <= 94)
+    return "Nyt alkaa näyttämään jo hyvältä. Pisteesi: $score";
+
+  else
+    return "Hurraa!! Sait kaikki oikein. Pisteesi: $score";
+}
+
+class Summary extends StatelessWidget {
+  final int score;
+
+  Summary({Key key, @required this.score}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery
+        .of(context)
+        .size
+        .width;
+    double screenHeight = MediaQuery
+        .of(context)
+        .size
+        .height;
+    double buttonsWidth = screenWidth / 3;
+    double buttonHeight = screenHeight / 10;
+
+    // TODO: implement build
+    return new WillPopScope(child: Scaffold(
+
+      body: new Container(
+
+          decoration: new BoxDecoration(
+              image: new DecorationImage(
+                  image: new AssetImage("images/Halli2.jpg"),
+                  colorFilter: new ColorFilter.mode(
+                      Colors.white.withOpacity(0.15), BlendMode.dstATop)
+                //fit: BoxFit.cover,
+              )
+          ),
+
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+
+            children: <Widget>[
+
+              new Text(scoreText(score) + "\n\n\n\n",
+                textAlign: TextAlign.center,
+                style: new TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+
+              new MaterialButton(
+                color: Colors.lightGreen,
+                minWidth: buttonsWidth,
+                height: buttonHeight,
+                onPressed: () {
+                  questionNumber = 0;
+                  finalScore = 0;
+                  Navigator.pop(context);
+                },
+                child: new Text("Yritä uudelleen",
+                  style: TextStyle(
+                      fontSize: buttonsWidth / 7,
+                      color: Colors.white
+                  ),
+                ),
+              ),
+
+              new Text("\n\n\n"),
+
+
+              new MaterialButton(
+                  color: Colors.green,
+                  minWidth: buttonsWidth,
+                  height: buttonHeight,
+                  child: new Text("Palaa valikkoon",
+                    style: TextStyle(
+                      fontSize: buttonsWidth / 7,
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () {
+                    TriviaState triviaState = new TriviaState();
+                    triviaState.resetQuiz();
+                  }
+              ),
+
+              new Text("\n\n\n"),
+
+            ],
+          )
+
+      ),
+
+    ),
+
+        onWillPop: () async => false);
+  }
+
+}
---
+++ game_game/lib/mainTrivia.dart
@@ -0,0 +1,194 @@
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import './Trivia.dart';
+
+void main(){
+  runApp(
+    new MaterialApp(
+      home: new triviaApp(),
+    )
+  );
+}
+
+class globals{
+  static String contents = "";
+  static String answers = "";
+}
+
+class triviaApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState(){
+    return new QuizState();
+  }
+}
+
+class QuizState extends State<triviaApp> {
+
+  String kysymysLista = "\nMikä on kuvassa?;\nMontako pelaajaa kentällä voi olla enintään?;\nMikä on Larry Poundsin pelinumero KTP-Basketissa?;\nKuinka monta raitaa on koripallossa?;\nMitkä ovat kansainvälisen koripalloliiton säännöissä koripallokentän mitat?";
+
+  @override
+  Widget build(BuildContext context) {
+
+    writeQuestions(kysymysLista);
+    readQuestions();
+
+    // TODO: implement build
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text("Trivia"),
+        backgroundColor: Colors.green,
+      ),
+
+      body: new Container(
+        decoration: new BoxDecoration(
+            image: new DecorationImage(
+                image: new AssetImage("images/Halli2.jpg"),
+                colorFilter: new ColorFilter.mode(
+                    Colors.white.withOpacity(0.15), BlendMode.dstATop)
+              //fit: BoxFit.cover,
+            )
+        ),
+
+        margin: const EdgeInsets.all(15.0),
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              "Vastaa kysymyksiin oikein ja voita huikea palkinto :D \n\n",
+              textAlign: TextAlign.center,
+              style: new TextStyle(
+                fontSize: 20.0,
+
+              ),
+            ),
+
+            new MaterialButton(
+              height: 100.0,
+              color: Colors.green,
+              onPressed: startQuiz,
+              child: new Text(
+                "Trivia",
+                style: new TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+
+
+            /*
+            new MaterialButton(
+              height: 100.0,
+              color: Colors.green,
+              onPressed: (){
+                writeQuestions(kysymysLista);
+              },
+              child: new Text(
+                "Kirjoita",
+                style: new TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+            new MaterialButton(
+              height: 100.0,
+              color: Colors.green,
+              onPressed: (){
+                getContents = globaalii.contents;
+                setState(() {
+                  readQuestions();
+                });
+              },
+              child: new Text(
+                "Lue",
+                style: new TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+            new Text(globaalii.contents),
+            */
+
+            /*
+            new IconButton(
+                padding: EdgeInsets.fromLTRB(200, 350.0, 10, 0),
+                icon: Icon(
+                  Icons.arrow_back,
+                  size: 100.0,
+                ),
+                color: Colors.red,
+                tooltip: "Score list",
+                onPressed: backBtn
+            ),
+            */
+
+          ],
+        ),
+      ),
+    );
+  }
+
+  //Method to Start Quiz
+  void startQuiz() {
+    setState(() {
+      Navigator.push(
+          context, new MaterialPageRoute(builder: (context) => new Trivia()));
+    });
+  }
+
+    // Move these file management functions to own dart file
+    // Find path to file
+    Future<String> get localPath async {
+      final directory = await getApplicationDocumentsDirectory();
+
+      return directory.path;
+    }
+
+    // Create reference to file location
+    Future<File> get localFile async {
+      final path = await localPath;
+      return File('$path/questions.txt');
+    }
+
+    Future<File> writeQuestions(String questions) async {
+      final file = await localFile;
+
+      // Write the file
+      return file.writeAsString(questions);
+    }
+
+  Future<String> readQuestions() async {
+      try {
+        final file = await localFile;
+
+        // Read the file
+        globals.contents = await file.readAsString();
+        //print(globaalii.contents);
+
+        return globals.contents.toString();
+      }
+      catch (e) {
+        // If encountering an error, return empty string
+        return "";
+      }
+    }
+}
+
+void backBtn() {
+  print("Back");
+
+  new Container(
+    child: new Column(
+      //crossAxisAlignment: CrossAxisAlignment.stretch,
+      //mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+
+        new Text(
+          "Back",
+          textAlign: TextAlign.center,
+        ),
+      ],
+    ),
+  );
+}
