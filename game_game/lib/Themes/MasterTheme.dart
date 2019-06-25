8522f9478bf8152d5a840fb59e9e147e96572908
Janessa <paradox.mochi@gmail.com>
2019-06-25 11:19:07 +0300

    creating a master theme

---
+++ game_game/lib/Themes/MasterTheme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MasterTheme {
+  static Brightness brightness = Brightness.dark;
+  static Color primaryColour = Colors.white;
+  static Color accentColour = Color.fromARGB(255, 0, 127, 0);
+
+  static String bestFont = '';
+
+  static double headlineSize = 70;
+  static double titleSize = 40;
+  static double bodySize = 15;
+
+  static ThemeData developmentTheme() {
+    return ThemeData(
+      brightness: brightness,
+      primaryColor: primaryColour,
+      accentColor: accentColour,
+      fontFamily: bestFont,
+      textTheme: TextTheme(
+          headline:
+              TextStyle(fontSize: headlineSize, fontWeight: FontWeight.normal),
+          title: TextStyle(fontSize: titleSize, fontWeight: FontWeight.bold),
+          body1: TextStyle(fontSize: bodySize, fontWeight: FontWeight.normal)),
+    );
+  }
+}
