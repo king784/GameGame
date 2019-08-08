92f48969d9510e6019fd8878b1daa0ff588f229d
king784 <teemu.turku@pp2.inet.fi>
2019-08-08 15:19:19 +0300

    Update main.dart

--- game_game/lib/main.dart
+++ game_game/lib/main.dart
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/widgets.dart';
 
 void main() => runApp(const Center(child: Text('Hello, world!', textDirection: TextDirection.ltr)));
-=======
-int main(){
-  
-}
->>>>>>> notificeissonii
