4cd8b553cc660b75e11fc1d2d8fd174629c5a600
king784 <teemu.turku@pp2.inet.fi>
2019-08-12 11:59:49 +0300

    toimii

---
+++ game_game/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\teemu.turku\Documents\GitHub\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\teemu.turku\Documents\GitHub\GameGame\game_game"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\teemu.turku\Documents\GitHub\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
---
+++ game_game/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_testuu/Themes/MasterTheme.dart';
+
+import 'route_generator.dart';
+import 'pages/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: MasterTheme.mainTheme,
+      title: 'Game Game Messis',
+      home: Home(),
+      initialRoute: '/startPage',
+      onGenerateRoute: RouteGenerator.generateRoute,
+    );
+  }
+}
