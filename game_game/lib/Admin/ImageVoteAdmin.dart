c49e1a8730aba3442cdbf573a8d37b1ebb4a2848
RikuH <riku_hei@hotmail.com>
2019-09-17 15:53:39 +0300

    jee

--- game_game/lib/Admin/ImageVoteAdmin.dart
+++ game_game/lib/Admin/ImageVoteAdmin.dart
@@ -1,9 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intl/intl.dart';
 
-import 'Globals.dart';
+import '../Globals.dart';
+import '../imageFromDB.dart';
 
 //make things work, gamecode check is shitty
 
@@ -138,7 +141,72 @@ class _ImageVotingAdminState extends State<ImageVotingAdmin> {
     });
   }
 
-  void getBestImageWinners() {}
+  void getBestImageWinners() async
+  {
+    //DIIBADAABA...
+    setState(() {
+      deletingText = "Siirretään voitto kuvaa talteen";
+    });
 
-  void deleteOldImages() {}
+      final QuerySnapshot imgRef = await Firestore.instance
+          .collection('imagesForBestImageVoting').orderBy("totalVotes", descending: true).getDocuments();
+
+        int bestVotes = imgRef.documents[0]['totalVotes'];
+
+        List<ImageFromDB> bestImages = new List<ImageFromDB>();
+
+        for(int i = 0; i < imgRef.documents.length; i++)
+        {
+          if(imgRef.documents[i]['totalVotes'] == bestVotes)
+          {
+            bestImages.add(new ImageFromDB.fromJson(imgRef.documents[i].data));
+            bestImages[i].isWinning = true;
+          }
+        }
+
+        if (bestVotes <= 0){
+          return;
+        }
+
+        String todayInString = DateFormat("dd-MM-yyyy").format(DateTime.now()).toString();
+
+        FirebaseStorage storage = FirebaseStorage.instance;
+        var refToImg;
+        StorageReference reference;
+        List<String> tempString = new List<String>();
+        // Add images which won to the database
+        for(int i = 0; i < bestImages.length; i++)
+        {
+          tempString.clear();
+          tempString = bestImages[i].imgUrl.split('/');
+          
+          // image
+          // refToImg = FirebaseStorage.instance.ref().child(bestImages[i].imgUrl);
+          // var url = await refToImg.getDownloadURL();
+          // Image img = Image.network(url);
+          
+          // reference =
+          //   _storage.ref().child('VictoryPictures/').child(todayInString).child(tempString[1]);
+          //   StorageUploadTask uploadTask = reference.putFile(imgFile);
+
+            DocumentReference bestImagesRef = Firestore.instance.collection("WinningImagesForImageVoting").document();
+            bestImagesRef.get().then((DocumentSnapshot ds) {
+            bestImagesRef.setData({
+            'dateTaken': bestImages[i].dateTaken,
+            'downloadUrl': bestImages[i].downloadUrl,
+            'imgUrl': bestImages[i].imgUrl,
+            'photographerName': bestImages[i].photographerName,
+            'totalVotes': bestImages[i].totalVotes,
+            'isWinning': bestImages[i].isWinning
+          }, merge: true);
+          });
+
+  }
+
+  void deleteOldImages() 
+  {
+    setState(() {
+      deletingText = "Poistetaan kuvia";
+    });
+  }
 }
